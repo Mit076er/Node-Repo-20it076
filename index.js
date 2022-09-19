e06726259d6e0466bbd75f8e4a30ec2fbd9a3f88
shlok55 <20it045@charusat.edu.in>
2022-09-19 14:09:52 +0530

    Committed by 076

---
+++ index.js
@@ -0,0 +1,10 @@
+const calculator = require('./app');
+
+let r  = calculator.sum(5,6)
+console.log(r)
+let s  = calculator.sub(8,3)
+console.log(s)
+let t  = calculator.multi(4,7)
+console.log(t)
+let u  = calculator.divide(8,2)
+console.log(u)
